b249d2113af13f6b3f60941d79cab91aa1dc727e
TunaSandwinch <winch.mascarenas@gmail.com>
2025-04-28 17:26:21 +0800

    refactor:add a condition where the method will return the same array if the set of moves is empty

--- lib/modules/moves_validator.rb
+++ lib/modules/moves_validator.rb
@@ -4,6 +4,8 @@ require_relative '../../lib/pieces/king'
 # a module for validating the set of moves for each piece
 module MovesValidator
   def remove_invalid_last_move(set_of_moves, board)
+    return set_of_moves if set_of_moves.empty?
+
     last_move = board.grid[set_of_moves.last[0]][set_of_moves.last[1]]
     return set_of_moves if last_move == ' '
 
