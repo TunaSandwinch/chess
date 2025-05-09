e607526932eee60fe3043d99cafdf6281cafc421
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-09 21:49:17 +0800

    feat: add method toidentify all possible moves

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -8,4 +8,15 @@ class Knight
     @piece = color.zero? ? "\u2658" : "\u265E"
     @color = color
   end
+
+  def possible_moves(position)
+    addends = [[-2, -1], [-2, 1], [-1, 2], [1, 2], [-1, -2], [1, -2], [2, -1], [2, 1]]
+    moves = []
+    addends.each do |addend1, addend2|
+      row = position[:row] + addend1
+      column = position[:column] + addend2
+      moves << [row, column] if row.between?(0, 7) && column.between?(0, 7)
+    end
+    moves
+  end
 end
