eaf1d4a7dc5efdebb0759eae99b21ffb7cd09bda
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-11 22:16:34 +0800

    refactor: rename type instance variable into color for better intuition

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -2,10 +2,10 @@
 
 # a class for all bishop related operations
 class Bishop
-  attr_accessor :piece, :type
+  attr_accessor :piece, :color
 
-  def initialize(type)
-    @piece = type.zero? ? "\u2657" : "\u265D"
-    @type = type
+  def initialize(color)
+    @piece = color.zero? ? "\u2657" : "\u265D"
+    @color = color
   end
 end
