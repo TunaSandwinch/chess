c3020a8d11de9e102eee29238a34ae545e0911fb
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-13 13:07:45 +0800

    feat: add method to determine all valid moves per move direction

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -8,4 +8,17 @@ class Bishop
     @piece = color.zero? ? "\u2657" : "\u265D"
     @color = color
   end
+
+  def moves(increment, position, board)
+    valid_moves = []
+    row, column = position.values_at(:row, :column)
+    while row.between?(1, 6) && column.between?(1, 6)
+      row += increment[:row]
+      column += increment[:column]
+      tile = board.grid[row][column]
+      valid_moves << [row, column]
+      return (tile.color == color ? valid_moves.tap(&:pop) : valid_moves) if tile.respond_to?(:color)
+    end
+    valid_moves
+  end
 end
