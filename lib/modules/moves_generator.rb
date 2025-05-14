1ae1b5747c3238bb009651f7fbacd10dd81d786d
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-14 15:34:28 +0800

    refactor: change the module functionality into generating moves based on its direction(increment)

---
+++ lib/modules/moves_generator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/pieces/king'
+# a module for validating the set of moves for each piece
+module MovesGenerator
+  def moves(increment, position, board)
+    valid_moves = []
+    row, column = position.values_at(:row, :column)
+    while row.between?(1, 6) && column.between?(1, 6)
+      row += increment[:row]
+      column += increment[:column]
+      tile = board.grid[row][column]
+      valid_moves << [row, column]
+      return (tile.color == color ? valid_moves.tap(&:pop) : valid_moves) if tile.respond_to?(:color)
+    end
+    valid_moves
+  end
+end
