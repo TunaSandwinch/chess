aa5bb40a600058cba28e1d0c927843c684054d81
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-14 23:38:50 +0800

    refactor: add moves generator module to the class

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,31 +1,20 @@
 # frozen_string_literal: true
 
+require_relative '../modules/moves_generator'
 # a class for all bishop related operations
 class Bishop
   attr_accessor :piece, :color
 
+  include MovesGenerator
   def initialize(color)
     @piece = color.zero? ? "\u2657" : "\u265D"
     @color = color
   end
 
-  def moves(increment, position, board)
-    valid_moves = []
-    row, column = position.values_at(:row, :column)
-    while row.between?(1, 6) && column.between?(1, 6)
-      row += increment[:row]
-      column += increment[:column]
-      tile = board.grid[row][column]
-      valid_moves << [row, column]
-      return (tile.color == color ? valid_moves.tap(&:pop) : valid_moves) if tile.respond_to?(:color)
-    end
-    valid_moves
-  end
-
   def available_moves(position, board)
     increments = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
     valid_moves = []
-    increments.each { |increment| valid_moves + moves(increment, position, board) }
+    increments.each { |increment| valid_moves += moves(increment, position, board) }
     valid_moves
   end
 end
