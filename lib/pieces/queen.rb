7b911b9ac6787bc377e245297a1418a87e74f7f6
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-15 21:17:42 +0800

    feat: add method to determine all available moves

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -4,12 +4,16 @@ require_relative '../pieces/bishop'
 require_relative '../pieces/rook'
 # a class for all queen related operations
 class Queen
-  attr_accessor :piece, :type
+  attr_accessor :piece, :color, :bishop, :rook
 
   def initialize(color)
     @piece = color.zero? ? "\u2655" : "\u265B"
     @color = color
     @bishop = Bishop.new(color)
     @rook = Rook.new(color)
+  end
+
+  def available_moves(position, board)
+    bishop.available_moves(position, board) + rook.available_moves(position, board)
   end
 end
