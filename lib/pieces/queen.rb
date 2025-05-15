e1f3427a380cd19a7a9c3b1342b44ed8a7092b50
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-15 16:28:08 +0800

    refactor: rename type instance variable into color and add bishop and rook objects into the class

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require_relative '../pieces/bishop'
+require_relative '../pieces/rook'
 # a class for all queen related operations
 class Queen
   attr_accessor :piece, :type
 
-  def initialize(type)
-    @piece = type.zero? ? "\u2655" : "\u265B"
-    @type = type
+  def initialize(color)
+    @piece = color.zero? ? "\u2655" : "\u265B"
+    @color = color
+    @bishop = Bishop.new(color)
+    @rook = Rook.new(color)
   end
 end
