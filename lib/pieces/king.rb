b964858f32ea10869d654a680a444ff2553282e1
TunaSandwinch <winch.mascarenas@gmail.com>
2025-05-16 13:13:35 +0800

    feat: add method to determine inrange moves

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative 'bishop'
+require_relative 'knight'
+require_relative 'rook'
 # a class for all king related operations
 class King
   attr_accessor :piece, :color, :current_position
@@ -8,5 +11,20 @@ class King
     @piece = color.zero? ? "\u2654" : "\u265A"
     @color = color
     @current_position = initial_position
+    @rook = Rook.new(color)
+    @bishop = Bishop.new(color)
+    @knight = Knight.new(color)
+  end
+
+  def inrange_moves
+    increments = [[-1, 0], [0, 1], [1, 0], [0, -1], [-1, 1], [1, 1], [1, -1], [-1, -1]]
+    moves = []
+    increments.each do |row_step, column_step|
+      row = current_position[:row] + row_step
+      column = current_position[:column] + column_step
+      moves << [row, column] if row.between?(0, 7) && column.between?(0, 7)
+    end
+    moves
   end
 end
+# do an in range moves method and then possible moves method then do a check? method
